eb5ef114aa7c8f1f83a2a0b5c9012d0f845fb5c7
hesan <hesanghasemi99@gmail.com>
2019-03-07 10:49:53 +0330

    UNITED UNITED

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hail ManU");
-        System.out.println("Underdog PSG");
+        System.out.println("UNITED UNITED");
+        System.out.println("FUCK PSG");
     }
 }
