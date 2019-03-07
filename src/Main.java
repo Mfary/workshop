5bd910031f0e9620c048bcd1bf524bec85bd26ae
amm266 <a.m.mohammadi@gmail.com>
2019-03-07 10:48:58 +0330

    football guys

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hail ManU");
+        System.out.println("GGMU");
         System.out.println("Underdog PSG");
+        system.out.println("more than a club");
     }
 }
