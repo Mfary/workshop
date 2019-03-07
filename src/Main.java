5aa406ad4811830459190a654e23d15ef7337cb1
mfary <moha1379>
2019-03-07 10:57:27 +0330

    debug system.out

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("GGMU");
         System.out.println("Underdog PSG");
-        system.out.println("more than a club");
+        System.out.println("more than a club");
     }
 }
