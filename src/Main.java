9834c705a728bbdac4732580ecb5cc0c6e64e5d3
hesan <hesanghasemi99@gmail.com>
2019-03-07 09:53:42 +0330

    fuckboy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("fuck good boy workshop");
+        System.out.println("fuck good boy workshop motherfucker piece of shit cunt");
     }
 }
